eff0d6db4822133e20014818e35ae07ddf0b5d69
ashoordoushow <ashoor12323@gmail.com>
2024-11-07 09:33:06 -0600

    Add users create action (signup)

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/exercises" => "exercises#index"
+  
   get "/routines" => "routines#index"
+
+  post "/users" => "users#create"
 
   # Defines the root path route ("/")
   # root "posts#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_07_031433) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_07_152015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_07_031433) do
     t.integer "exercise_id"
     t.integer "reps"
     t.string "sets"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
