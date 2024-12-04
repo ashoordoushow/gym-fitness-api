2b0a2f416e3c5ac2ff7a4dbf7a2010af70bfc8c3
ashoordoushow <ashoor12323@gmail.com>
2024-12-03 19:25:18 -0600

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "localhost:5173", "gym-fitness-frontend.onrender.com"
+    origins "example.com", "localhost:5173", "built-to-conquer.up.railway.app"
     resource "*", headers: :any, credentials: true, methods: [:get, :post, :patch, :put, :delete]
   end
 end
